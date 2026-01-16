e10a1d2214c265f92bddc663aafe5f32c5bb05f2
chuanlong <841290355@qq.com>
2026-01-17 01:47:24 +0800

    feat: 前端基本框架搭建

---
+++ shen-ai-frontend/src/pages/HomePage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="homePage">111</div>
+</template>
+
+<style scoped>
+#homePage {
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
+  position: relative;
+  overflow: hidden;
+}
+</style>
