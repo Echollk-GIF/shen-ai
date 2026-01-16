8504f88ba455506680d0609282a425571a68f548
chuanlong <841290355@qq.com>
2026-01-16 23:35:08 +0800

    Initial commit

---
+++ src/main/java/com/chuanlong/shenai/ShenAiApplication.java
@@ -0,0 +1,13 @@
+package com.chuanlong.shenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShenAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShenAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chuanlong/shenai/ShenAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuanlong.shenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShenAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
