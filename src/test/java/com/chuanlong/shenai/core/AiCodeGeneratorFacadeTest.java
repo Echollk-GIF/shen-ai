b6a1cc724607e174ee1676792b3aa9e64dbc6070
chuanlong <841290355@qq.com>
2026-01-20 04:33:01 +0800

    feat: 优化代码结构

--- src/test/java/com/chuanlong/shenai/core/AiCodeGeneratorFacadeTest.java
+++ src/test/java/com/chuanlong/shenai/core/AiCodeGeneratorFacadeTest.java
@@ -5,8 +5,10 @@ import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 
 import java.io.File;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,7 +20,19 @@ class AiCodeGeneratorFacadeTest {
 
     @Test
     void generateAndSaveCode() {
-        File file = aiCodeGeneratorFacade.generateAndSaveCode("任务记录网站,不超过10行", CodeGenTypeEnum.MULTI_FILE);
+        File file = aiCodeGeneratorFacade.generateAndSaveCode("生成一个登录页面，总共不超过 20 行代码", CodeGenTypeEnum.MULTI_FILE);
         Assertions.assertNotNull(file);
+    }
+
+    @Test
+    void generateAndSaveCodeStream() {
+        Flux<String> codeStream = aiCodeGeneratorFacade.generateAndSaveCodeStream("生成一个登录页面，总共不超过 20 行代码", CodeGenTypeEnum.HTML);
+        // 阻塞等待所有数据收集完成
+        List<String> result = codeStream.collectList().block();
+        // 验证结果
+        Assertions.assertNotNull(result);
+        // 拼接字符串，得到完整内容
+        String completeContent = String.join("", result);
+        Assertions.assertNotNull(completeContent);
     }
 }
